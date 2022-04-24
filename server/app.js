11e5c1260d5f59bb83dbe0d248a639633fe02e5a
MohanadOO <moohand220@gmail.com>
2022-04-25 00:39:04 +0300

    Add mongoose

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect(process.env.MONGO_URI)
+mongoose.connection.once('open', () => {
+  console.log('Connected to database')
+})
 
 app.use(
   '/graphql',
