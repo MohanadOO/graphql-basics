ddc14d2d1ff96918f06d2248c2a6bfe50c6c7b1b
MohanadOO <moohand220@gmail.com>
2022-04-25 02:00:53 +0300

    fix spelling

--- models/book.js
+++ models/book.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const bookSchema = new Schema({
-    name: String,
-    genre: String, 
-    authorid: String
+  name: String,
+  genre: String,
+  authorid: String,
 })
 
-models.exports = mongoose.model('Book', bookSchema)
+module.exports = mongoose.model('Book', bookSchema)
