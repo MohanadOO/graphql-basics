9cb2111065880573452cd0b4061af3e5352ced16
MohanadOO <moohand220@gmail.com>
2022-04-25 23:26:59 +0300

    Move quires to seperate file

---
+++ client/src/queries/queries.js
@@ -0,0 +1,30 @@
+import { gql } from 'apollo-boost'
+
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`
+
+const getAuthorsQuery = gql`
+  {
+    authors {
+      name
+      id
+    }
+  }
+`
+
+const addBookMutation = gql`
+  mutation ($name: String!, $genre: String!, $authorid: ID!) {
+    addBook(name: $name, genre: $genre, authorid: $authorid) {
+      name
+      id
+    }
+  }
+`
+
+export { getAuthorsQuery, getBooksQuery, addBookMutation }
