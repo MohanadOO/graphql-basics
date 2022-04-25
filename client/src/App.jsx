87ce5e2b85fce37fa830ddb7d68d29596ec9974f
MohanadOO <moohand220@gmail.com>
2022-04-25 23:28:59 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,28 @@
 import { useState } from 'react'
 import BookList from './components/BookList'
+import AddBook from './components/AddBook'
 import ApolloClient from 'apollo-boost'
-import { ApolloProvider } from '@apollo/react-hooks'
+import { ApolloProvider } from 'react-apollo'
+import { PlusCircleIcon } from '@heroicons/react/solid'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3000/graphql',
+  uri: 'http://localhost:5500/graphql',
 })
 
 function App() {
+  const [addBook, setAddBook] = useState(false)
   return (
     <ApolloProvider client={client}>
       <div className='App'>
         <h1>Reading List</h1>
         <BookList />
+        {!addBook ? (
+          <PlusCircleIcon
+            className='plus-icon'
+            onClick={() => setAddBook(true)}
+          />
+        ) : null}
+        {addBook ? <AddBook /> : null}
       </div>
     </ApolloProvider>
   )
